c1c35ef206a5e2b511b345e007f8df42d16f7277
Tony Huang <r04922101@ntu.edu.tw>
2021-12-26 00:45:06 +0800

    construct cookie header for clapPhoto function

--- v2/background.js
+++ v2/background.js
@@ -1,11 +1,42 @@
-// listen to a message from content scriipt
-chrome.runtime.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(message) {
-    const { start } = message;
-    if (start) {
-      // start clapping album
-    }
+// listen to a message from content script
+const domain = ".biji.co";
+let headerCookies;
+(async function constructCookieHeader() {
+  const cookies = await chrome.cookies.getAll({ domain });
+  headerCookies = cookies
+    .map((cookie) => `${cookie.name}=${cookie.value}`)
+    .join("; ");
+})();
+
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(async function (message) {
+    const { albumId } = message;
     // TODO: send back progress
     // port.postMessage({question: "Who's there?"});
+    return true;
   });
 });
+
+async function clapPhoto(albumId, apId, headerCookies) {
+  return fetch("https://hiking.biji.co/album/ajax/clap_photo", {
+    headers: {
+      accept: "*/*",
+      "accept-language": "en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7",
+      "content-type": "text/plain;charset=UTF-8",
+      "sec-ch-ua":
+        '"Microsoft Edge";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"macOS"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      cookies: headerCookies,
+    },
+    referrer: `https://hiking.biji.co/index.php?q=album&act=photo&album_id=${albumId}&ap_id=${apId}`,
+    referrerPolicy: "no-referrer-when-downgrade",
+    body: `{"id":"${apId}"}`,
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+  }).then((resp) => resp.json());
+}
